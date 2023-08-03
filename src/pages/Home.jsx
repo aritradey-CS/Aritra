b937688f13de4551d762f204f9905eea219b42d1
aritradey-CS <aritra1999.dey@gmail.com>
2023-08-03 22:21:00 +0530

    adding some freature and they are working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,31 @@
-import React, { useState } from 'react';
-import './Home.css';
-import { BlogCardList } from '../pages/Blog'; // Adjust the import path
-import { animateScroll as scroll } from 'react-scroll'; // Import react-scroll
-
+import React, { useState } from "react";
+import "./Home.css";
+import { BlogCardList } from "../pages/Blog"; // Adjust the import path
+import { animateScroll as scroll } from "react-scroll"; // Import react-scroll
 
 const initialTopBlogs = [
-  { title: 'Top Blog 1', content: 'Lorem ipsum dolor sit amet.' },
-  { title: 'Top Blog 2', content: 'Consectetur adipiscing elit.' },
+  { title: "Top Blog 1", content: "Lorem ipsum dolor sit amet." },
+  { title: "Top Blog 2", content: "Consectetur adipiscing elit." },
   // Add more top blog objects
 ];
 
 const additionalBlogs = [
   {
-    title: 'Additional Blog 1',
-    content: 'Additional content for Blog 1 goes here.',
+    title: "Additional Blog 1",
+    content: "Additional content for Blog 1 goes here.",
   },
   {
-    title: 'Additional Blog 2',
-    content: 'Additional content for Blog 2 goes here.',
+    title: "Additional Blog 2",
+    content: "Additional content for Blog 2 goes here.",
   },
 
   {
-    title: 'Additional Blog 3',
-    content: 'Additional content for Blog 1 goes here.',
+    title: "Additional Blog 3",
+    content: "Additional content for Blog 1 goes here.",
   },
   {
-    title: 'Additional Blog 4',
-    content: 'Additional content for Blog 2 goes here.',
+    title: "Additional Blog 4",
+    content: "Additional content for Blog 2 goes here.",
   },
   // Add more demo blogs as needed
   // Add more demo blogs as needed
@@ -43,7 +42,7 @@ function Home() {
     }
     setExpanded(true);
   };
-  
+
   const handleCollapseClick = () => {
     setBlogs(initialTopBlogs);
     setExpanded(false);
@@ -53,13 +52,18 @@ function Home() {
     <div className="home-container">
       <h2 className="home-title">Welcome to My Blog!</h2>
       <p className="home-content">
-        This is the home page content. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-        vel urna quis felis posuere interdum a non erat.
+        This is the home page content. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Sed vel urna quis felis posuere interdum a non erat.
       </p>
       <button className="home-button" onClick={handleExploreClick}>
         Explore
       </button>
-
+      <button
+        className="home-button"
+        onClick={() => handleCollapseClick(setBlogs, setExpanded)}
+      >
+        Collapse
+      </button>
       <h3>Top Blogs</h3>
       <BlogCardList topBlogs={blogs} />
     </div>
