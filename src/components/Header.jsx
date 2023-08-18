c39c51c627df3a9731ab3c062a00941d231872c6
aritradey-CS <aritra1999.dey@gmail.com>
2023-08-18 21:25:35 +0530

    Works on Header.jsx and modifing and adding some thing new

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,32 @@
-// Example for animating the header
 import React from 'react';
 import { useSpring, animated } from 'react-spring';
 import './Header.css';
+import profileImage from '../assets/profile.jpg'; // Import your profile image
 
 function Header() {
   const props = useSpring({ opacity: 1, from: { opacity: 0 } });
 
   return (
     <animated.header style={props}>
-      <h1>My Personal Blog</h1>
+      <div className="header-container">
+        <div className="profile-image-container">
+          <a href="/" className="profile-image-link">
+            <img src={profileImage} alt="Profile" className="profile-image" />
+          </a>
+        </div>
+        <div className="profile-details">
+          <h1>Aritra Dey</h1>
+          <p>aritra1999.dey@gmail.com <br /> 8582970748</p>
+          <div className="social-links">
+            <a href="https://github.com/aritradey-CS" target="_blank" rel="noopener noreferrer">
+              GitHub
+            </a>
+            <a href="https://www.linkedin.com/in/aritra-dey-13552418b/" target="_blank" rel="noopener noreferrer">
+              LinkedIn
+            </a>
+          </div>
+        </div>
+      </div>
     </animated.header>
   );
 }
