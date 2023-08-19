c8be3ee025e199c72b8065485c1f564ba9027917
aritradey-CS <aritra1999.dey@gmail.com>
2023-08-19 11:15:22 +0530

    work on header design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import "./Home.css";
 import { BlogCardList } from "../pages/Blog"; // Adjust the import path
 import { animateScroll as scroll } from "react-scroll"; // Import react-scroll
+import { useSpring, animated } from "react-spring"; // Import react-spring
+
 
 const initialTopBlogs = [
   { title: "Top Blog 1", content: "Lorem ipsum dolor sit amet." },
@@ -47,15 +49,24 @@ function Home() {
     setBlogs(initialTopBlogs);
     setExpanded(false);
   };
+  // Define the animation spring for the Collapse button
+  const collapseButtonProps = useSpring({
+    opacity: expanded ? 1 : 0, // Apply opacity based on expanded state
+    display: expanded ? "block" : "none", // Show button when expanded
+  });
+
+  const collapseButtonAnimationProps = useSpring({
+    transform: expanded ? "scale(1)" : "scale(0.5)",
+  });
 
   return (
     <div className="home-container">
       <h2 className="home-title">Welcome aspirants!</h2>
       <p className="home-content">
         I am an engineering student in the Department of Computer Science, I
-        have completed my diploma course in 2019 and graduate in 2022 now doing Master's. from my
-        blog, you can get informations for digital technologies which are may
-        included in your course or beyond!
+        have completed my diploma course in 2019 and graduate in 2022 now doing
+        Master's. from my blog, you can get informations for digital
+        technologies which are may included in your course or beyond!
       </p>
       <button
         className={`home-button ${expanded ? "expanded" : ""}`}
